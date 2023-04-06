4dbe438556958c957370a85793162e93ff4c631a
Michael Wood <michaelwood33311@icloud.com>
2023-04-06 12:17:56 -0400

    added independent function to send page_view event to GA4

--- sync.js
+++ sync.js
@@ -38,7 +38,7 @@ csv.parseCSV("geotargets.csv", function(data){
     
 }, false);
 
-async function sendG3PageView (hostname, url, clientId, title, userAgent = '', referrer = '', city = '', country = '') {
+async function sendG3PageView (hostname, url, clientId, title, userAgent = '', referrer = '',  country = '', city = '',) {
      const webPropertyId = hostname === 'gamma.pymnts.com' ? 'UA-11167465-10' : 'UA-11167465-1';
 
      const params = {
@@ -72,4 +72,65 @@ async function sendG3PageView (hostname, url, clientId, title, userAgent = '', r
     }
 }
 
-sendG3PageView('gamma.pymnts.com', '/test/url', '99c48053-7ec2-4898-b91f-4255502fb981', 'Test Url', 'Amazing Browser');
+async function sendG4PageView (hostname, url, deviceId, timeOnPage, title = '', userAgent = 'unknown', referrer = 'unknown', country = 'unknown', region = 'unknown', city = 'unknown') {
+    const g4Id = hostname === 'gamma.pymnts.com' ? 'G-NY60TDWHJ9' : 'G-3WHRCQ5780';
+    const apiSecret = hostname === 'gamma.pymnts.com' ? 'LSPWrwHwTyKhghOCL2PqRA' : 'dlnjCX6cQmSqk73YzmIXsg';
+      /*
+         * Send to GA4
+         */
+      params = {
+        api_secret: apiSecret,
+        measurement_id: g4Id,
+        uc: 'US'
+    }
+
+    let data = {
+        client_id: deviceId,
+        user_id: deviceId,
+        events: [
+            {
+                name: 'page_view',
+                params: {
+                    engagement_time_msec: timeOnPage,
+                    page_location: `https://${hostname}${url.indexOf('?') === -1 ? `${url}?ppp=true` : `${url}&ppp=true`}`,
+                    page_path: url.indexOf('?') === -1 ? `${url}?ppp=true` : `${url}&ppp=true`,
+                    page_title: title.replaceAll(' ', '-'),
+                    page_referrer: referrer
+                    
+                }
+            },
+            {
+                name: 'pymnts_rt_proxy',
+                params: {
+                    blocked_visitor: 1,
+                    user_agent: userAgent,
+                    country,
+                    region,
+                    city
+                }
+            }
+        ]
+    }
+
+    request = {
+        url: "https://www.google-analytics.com/mp/collect",
+        method: "post",
+        params,
+        data
+    }
+
+    console.log("G4 Request: ", JSON.stringify(request,null, 4));
+
+    let response;
+
+    try {
+        response = await axios(request);
+        console.log('G4 Response: ', response.data);
+    } catch (err) {
+        console.error('G4 Error: ', err);
+    }
+
+}
+
+//sendG3PageView('gamma.pymnts.com', '/test/url', '99c48053-7ec2-4898-b91f-4255502fb981', 'Test Url', 'Amazing Browser');
+sendG4PageView('gamma.pymnts.com', '/test/url/', '99c48053-7ec2-4898-b91f-4255502fb981', 60000, 'Test URL', 'Amazing Browser');
